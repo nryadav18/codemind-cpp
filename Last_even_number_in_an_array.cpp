be7976026a10a9c5afcf857ccaf697ee787cf134
Rajeswar Yadav <119839624+nryadav18@users.noreply.github.com>
2023-10-08 21:11:30 +0530

    #include<bits/stdc++.h> using namespace std; int main (){     //nry_code();     int n;     cin>>n;     int arr[n];     for (int i=0;i<n;i++){         cin>>arr[i];     }     for (int i=n-1;i>=0;i--){         if (arr[i]%2==0){             cout << arr[i] << endl;             break;         }     }     return 0; }

---
+++ Last_even_number_in_an_array.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    //nry_code();
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for (int i=n-1;i>=0;i--){
+        if (arr[i]%2==0){
+            cout << arr[i] << endl;
+            break;
+        }
+    }
+    return 0;
+}
